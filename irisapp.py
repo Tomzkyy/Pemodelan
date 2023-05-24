ec2bea5dbc6e4b6416d4d7a566a9d2846813e90a
Muhammad Ariq Pratama <91134089+Tomzkyy@users.noreply.github.com>
2023-05-24 13:34:20 +0700

    Update title irisapp.py

--- irisapp.py
+++ irisapp.py
@@ -8,7 +8,7 @@ import numpy as np
 iris = datasets.load_iris()
 
 
-st.title("App Prediksi Iris")
+st.title("App Prediksi Iris dengan Algoritma KNN")
 
 img = Image.open("irisall.jpg")
 st.image(img, use_column_width=False)
